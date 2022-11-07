83c09ce8c014ebee36f6e7a6d3ffcb934a349819
wooju <dnwnrnt@naver.com>
2022-11-07 23:54:53 +0900

    RoundButton

---
+++ Caculator/RoundButton.swift
@@ -0,0 +1,19 @@
+//
+//  RoundButton.swift
+//  Caculator
+//
+//  Created by wooju on 2022/11/07.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+}
