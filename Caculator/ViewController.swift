ea1be73802acaeb9c9126e84e035a2876fbe2876
wooju <dnwnrnt@naver.com>
2022-11-08 00:01:46 +0900

    Button

--- Caculator/ViewController.swift
+++ Caculator/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case unknown
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func tapClearButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func tapDotButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapDivideButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapMultiplyButton(_ sender: UIButton) {
+    }
+    @IBAction func tapSubtractButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapEqualButton(_ sender: UIButton) {
+    }
+    
 }
 
